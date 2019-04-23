f9043a446814799fc411e7f451e4a02331f8666a
amiko <caihaifei@enjoytoday.cn>
2019-04-23 23:31:45 +0800

    update.

---
+++ test/src/main/java/cn/enjoytoday/shortvideo/test/ui/api/audio/AudioGatherActivity.kt
@@ -0,0 +1,13 @@
+package cn.enjoytoday.shortvideo.test.ui.api.audio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.enjoytoday.shortvideo.test.R
+
+class AudioGatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_gather)
+    }
+}
